291ddbe8f3af2d207ff3acba84513fef3b6bfff1
Evgenii Malikov <evgenii.malikov@taxmaro.com>
2021-03-21 22:19:39 +0100

    return key after adding

--- Storage.js
+++ Storage.js
@@ -9,7 +9,6 @@ class Storage {
     let key = '';
     for (let i = 0; i < this.keyLength; i++) {
       const randomIndex = Math.floor(Math.random() * allowedCharacters.length);
-      console.log(allowedCharacters.length);
       key += allowedCharacters[randomIndex];
     }
     return key;
@@ -18,6 +17,7 @@ class Storage {
   add(link) {
     const key = this.genRandomKey();
     this.shortUrls.set(key, link);
+    return key;
   }
 
   get(key) {
