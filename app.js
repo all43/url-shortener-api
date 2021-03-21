8257899a0a2b69784f35de215146451feee24b71
Evgenii Malikov <evgenii.malikov@taxmaro.com>
2021-03-22 00:36:33 +0100

    actual endpoints added

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+const Storage = require('./Storage');
+
+const storage = new Storage();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (_req, res) => {
-  res.json({ msg: 'Hello world!' });
+  res.json( { msg: 'OK' } );
+});
+
+app.get('/api/keys/:key', (req, res) => {
+  const { key } = req.params;
+  const url = storage.get(key);
+  const statusCode = url ? 200 : 404;
+  res.status(statusCode).json({ url });
+});
+
+app.post('/api/urls', (req, res) => {
+  const { url } = req.body;
+  const key = storage.add(url);
+  res.status(201).json({ key });
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+const server = app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+module.exports = { app, server };
