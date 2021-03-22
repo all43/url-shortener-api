b8d6cdc25d3cf776ebef68259a1d68daf5ba0cab
Evgenii Malikov <evgenii.malikov@taxmaro.com>
2021-03-22 09:30:19 +0100

    imporved tests

--- app.test.js
+++ app.test.js
@@ -1,33 +1,49 @@
 const request = require('supertest');
+
 process.env.PORT = 3337;
 const { app, server } = require('./app');
 
 describe('API functions properly', () => {
-
   afterAll((done) => {
     server.close(done);
   });
 
-  it('responds with json', () => {
-    return request(app)
-      .get('/')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200);
-  });
+  it('responds with json', () => request(app)
+    .get('/')
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(200));
 
-  it('responds with error to invalid url', () => {
-    return response = request(app)
+  it('responds with error to invalid url', () => request(app)
     .post('/api/urls')
     .send({ url: 'Scooby-Doo' })
-    .expect(400);
-  });
+    .expect(400));
 
   it('creates short url', async () => {
     const response = await request(app)
       .post('/api/urls')
       .send({ url: 'https://google.com/' });
+    expect(response).toMatchObject({
+      statusCode: 201,
+      body: {
+        key: expect.any(String),
+        shortUrl: expect.any(String),
+      },
+    });
+  });
+
+  it('redirects to proper full url', async () => {
+    const url = 'https://www.berlin.de/';
+    const response = await request(app)
+      .post('/api/urls')
+      .send({ url });
     expect(response.statusCode).toEqual(201);
     expect(response.body).toHaveProperty('key');
-  })
+
+    const { shortUrl } = response.body;
+    return request(shortUrl)
+      .get('')
+      .expect(302)
+      .expect('Location', url);
+  });
 });
