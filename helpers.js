b3078b0d64236b76df0bb07c0085ad3f65f1d22e
Evgenii Malikov <evgenii.malikov@taxmaro.com>
2021-03-22 11:13:25 +0100

    regex fixed

--- helpers.js
+++ helpers.js
@@ -1,5 +1,5 @@
 function checkUrl(url) {
-  const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
+  const regex = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
   return regex.test(url);
 }
 
